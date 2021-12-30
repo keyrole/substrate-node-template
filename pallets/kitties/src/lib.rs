631722d08f9ea8492e443946499b17cdeb19698f
andy <yijianan1426@gmail.com>
2021-12-30 18:35:54 +0800

    add create function and coompiled pass

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -18,6 +18,7 @@ pub mod pallet {
     #[pallet::config]
     pub trait Config: frame_system::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
     }
 
     #[pallet::pallet]
@@ -28,7 +29,7 @@ pub mod pallet {
     #[pallet::metadata(T::AccountId = "AccountId")]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
-
+        KittyCreate(T::AccountId, KittyIndex),
     }
 
     #[pallet::storage]
@@ -45,15 +46,49 @@ pub mod pallet {
 
     #[pallet::error]
     pub enum Error<T> {
-
+        KittiesCountOverflow,
     }
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
+        #[pallet::weight(0)]
+        pub fn create(origin: OriginFor<T>) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            let kitty_id = match Self::kitties_count() {
+                Some(id) => {
+                    ensure!(id != KittyIndex::max_value(), Error::<T>::KittiesCountOverflow);
+                    id
+                },
+                None => {
+                    1
+                }
+            };
+
+            let dna = Self::random_value(&who);
+
+            Kitties::<T>::insert(kitty_id, Some(Kitty(dna)));
+
+            Owner::<T>::insert(kitty_id, Some(who.clone()));
+
+            KittiesCount::<T>::put(kitty_id + 1);
+
+            Self::deposit_event(Event::KittyCreate(who, kitty_id));
+
+            Ok(())
+        }
 
     }
 
     impl<T: Config> Pallet<T> {
+        fn random_value(sender: &T::AccountId) -> [u8; 16] {
+            let payload = (
+                T::Randomness::random_seed(),
+                &sender,
+                <frame_system::Pallet<T>>::extrinsic_index(),
+            );
+            payload.using_encoded(blake2_128)
+        }
 
     }
 }
